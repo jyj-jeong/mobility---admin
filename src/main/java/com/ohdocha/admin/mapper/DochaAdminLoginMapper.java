d8d1f9ae7b3362c331a27fefac728f041c7f1148
jslee <jungsoon.lee@sharenshare.kr>
2020-09-17 09:15:19 +0900

    로그인 기능 수정

--- src/main/java/com/ohdocha/admin/mapper/DochaAdminLoginMapper.java
+++ src/main/java/com/ohdocha/admin/mapper/DochaAdminLoginMapper.java
@@ -6,5 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface DochaAdminLoginMapper {
+
     public DochaAdminDcUserInfoResponse chkLoginUserInfo(DochaAdminDcUserInfoRequest requestDto);
+
 }
